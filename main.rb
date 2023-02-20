5433e23126331af2b9601eb5feb59f7b022512ce
Garry3Fingers <www.vlad78@yandex.ru>
2023-02-20 21:46:15 +0300

    Create class GuessWord

--- main.rb
+++ main.rb
@@ -1,3 +1,10 @@
 # frozen_string_literal: true
 
+# This class send random word from the dictionary
+class GuessWord
+  attr_reader :dictionary
 
+  def initialize
+    @dictionary = File.open('google-10000-english-no-swears.txt').readlines.sample
+  end
+end
