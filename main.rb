f6f88409fcb4f8498a6e4b2f3916dbd8215421e0
Garry3Fingers <www.vlad78@yandex.ru>
2023-02-22 21:04:05 +0300

    Refactor class GuessWord

--- main.rb
+++ main.rb
@@ -2,9 +2,22 @@
 
 # This class send random word from the dictionary
 class GuessWord
-  attr_reader :dictionary
-
   def initialize
-    @dictionary = File.open('google-10000-english-no-swears.txt').readlines.sample
+    @dictionary = File.open('google-10000-english-no-swears.txt').readlines
+  end
+
+  private
+
+  def clean_dictionary
+    @dictionary.map { |word| word.split(//) }.each(&:pop).map(&:join)
+  end
+
+  public
+
+  def random_word
+    dictionary = clean_dictionary
+    dictionary.select { |word| (word.length >= 5) && (word.length <= 12) }.sample
   end
 end
+
+p word = GuessWord.new.random_word
