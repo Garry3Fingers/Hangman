c13e8c97bb9d55076c05dc071aed3ea90a23bb31
Garry3Fingers <www.vlad78@yandex.ru>
2023-03-05 17:58:30 +0300

    Create a file for the GuessWord class

--- lib/main.rb
+++ lib/main.rb
@@ -2,26 +2,7 @@
 
 require 'colorize'
 require 'json'
-
-# This class send random word from the dictionary
-class GuessWord
-  def initialize
-    @dictionary = File.open('../google-10000-english-no-swears.txt').readlines
-  end
-
-  private
-
-  def clean_dictionary
-    @dictionary.map { |word| word.split(//) }.each(&:pop).map(&:join)
-  end
-
-  public
-
-  def random_word
-    dictionary = clean_dictionary
-    dictionary.select { |word| (word.length >= 5) && (word.length <= 12) }.sample
-  end
-end
+require_relative 'guess_word'
 
 class InvalidInput < StandardError; end
 
